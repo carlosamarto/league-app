83ed996242ca688f1ed543f6aa7a241ea67f2828
Carlos Martinez <cmartinez@mcontigo.com>
2023-09-20 00:13:07 -0300

    update/wpi: get with nextUrl

--- src/app/api/summoner/route.js
+++ src/app/api/summoner/route.js
@@ -5,9 +5,7 @@ export async function GET(request) {
   try {
     const apiKey = "RGAPI-67b216b0-3b46-442c-be0b-cc88a3000f62";
 
-    const url = new URL(request.url);
-
-    const summonerName = url.searchParams.get("summonerName");
+    const summonerName = request.nextUrl.searchParams.get("summonerName");
 
     const response = await axios.get(`https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`, {
       headers: {
