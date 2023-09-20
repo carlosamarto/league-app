0d543a987dfd88a3fff5edd514d3635b24be8718
Carlos Martinez <cmartinez@mcontigo.com>
2023-09-20 00:55:56 -0300

    update/wpi: without axios

--- src/app/api/summoner/route.js
+++ src/app/api/summoner/route.js
@@ -1,21 +1,20 @@
-import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
-  try {
-    const apiKey = "RGAPI-67b216b0-3b46-442c-be0b-cc88a3000f62";
+  const apiKey = "RGAPI-67b216b0-3b46-442c-be0b-cc88a3000f62";
 
-    // const summonerName = request.nextUrl.searchParams.get("summonerName");
+  const { searchParams } = new URL(request.url);
 
-    const response = await axios.get(`https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Jordacor`, {
-      headers: {
-        "X-Riot-Token": apiKey,
-      },
-    });
+  const summonerName = searchParams.get("summonerName");
 
-    return NextResponse.json(response.data);
-  } catch (error) {
-    console.error("Error al buscar al invocador:", error);
-    return NextResponse.json({ error: error.message || "Something went wrong" });
-  }
+  const res = await fetch(`https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-Riot-Token": apiKey,
+    },
+  });
+
+  const summoner = await res.json();
+
+  return NextResponse.json({ summoner });
 }
--- src/components/search.js
+++ src/components/search.js
@@ -9,12 +9,27 @@ const Search = () => {
 
   const handleSearch = async () => {
     try {
-      const response = await axios.get(`/api/summoner?summonerName=${summonerName}`);
-      setSummonerData(response.data);
+      const apiKey = "TU_API_KEY_AQUI";
+      const url = `/api/summoner?summonerName=${summonerName}`;
+
+      const response = await fetch(url, {
+        headers: {
+          "X-Riot-Token": apiKey,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("La solicitud no fue exitosa");
+      }
+
+      const data = await response.json();
+      setSummonerData(data);
     } catch (error) {
       console.error("Error al buscar al invocador:", error);
     }
   };
+
+  console.log(summonerData);
 
   return (
     <div>
@@ -31,9 +46,9 @@ const Search = () => {
 
       {summonerData && (
         <div>
-          <h2>Nombre: {summonerData.name}</h2>
+          <h2>Nombre: {summonerData.summoner.name}</h2>
 
-          <p>Nivel: {summonerData.summonerLevel}</p>
+          <p>Nivel: {summonerData.summoner.summonerLevel}</p>
         </div>
       )}
     </div>
