20d34b3f623d6029915810d680af1c5b75ac6096
Carlos Martinez <cmartinez@mcontigo.com>
2023-09-20 00:35:00 -0300

    upadte/wpi: test

--- src/app/api/summoner/route.js
+++ src/app/api/summoner/route.js
@@ -5,9 +5,9 @@ export async function GET(request) {
   try {
     const apiKey = "RGAPI-67b216b0-3b46-442c-be0b-cc88a3000f62";
 
-    const summonerName = request.nextUrl.searchParams.get("summonerName");
+    // const summonerName = request.nextUrl.searchParams.get("summonerName");
 
-    const response = await axios.get(`https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`, {
+    const response = await axios.get(`https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Jordacor`, {
       headers: {
         "X-Riot-Token": apiKey,
       },
